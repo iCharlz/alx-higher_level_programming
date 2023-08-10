1d273ed10e3d762461cc3f9a9510a9b9fc30185a
“Charles <charlznwoko@yahoo.com>
2023-08-10 08:13:45 -0700

    0x00. Python - Hello, World

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is interpreted, interactive, object-oriented programming \ language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[23:-17] + str[:6]
+str = str[39:66] + str[-23:-17] + str[:6]
 print(str)
