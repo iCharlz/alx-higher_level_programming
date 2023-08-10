29a981e699ec55945f7755aa388dfee42da3c12f
“Charles <charlznwoko@yahoo.com>
2023-08-10 14:53:22 -0700

    0x02. Python - import & modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
+    """Match bytecode provided by iCharlz"""
     from magic_calculation_102 import add, sub
 
     if a < b:
@@ -11,4 +11,4 @@ def magic_calculation(a, b):
         return (c)
 
     else:
-        return(sub(a, b))
+        return (sub(a, b))
