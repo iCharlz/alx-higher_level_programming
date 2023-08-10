cdd61d39187e2756341ecfd1dec00b47f5d288cb
“Charles <charlznwoko@yahoo.com>
2023-08-10 06:56:30 -0700

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-# 100-print_tebahpla.py
+# Author -Charles Nwoko
 
-""""Print the alphabet in reverse order upper- and lower-case."""
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
     i = 32 if i == 0 else 0
-
