0d42bc5ae58dbd6bd33696c0534dcb5de6fac3f1
“Charles <charlznwoko@yahoo.com>
2023-08-16 03:51:10 -0700

    0x04. Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/0-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
+
+guillaume@ubuntu:~/0x04$ ./0-main.py
+[[1, 4, 9], [16, 25, 36], [49, 64, 81]]
+[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
