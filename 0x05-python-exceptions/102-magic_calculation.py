4af19cd955e0fb74a3ffff80a8c307ca9911e141
“Charles <charlznwoko@yahoo.com>
2023-08-28 10:12:03 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -8,7 +7,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             else:
-                result += a ** b / i
+                result += (a ** b) / i
         except:
             result = b + a
             break
