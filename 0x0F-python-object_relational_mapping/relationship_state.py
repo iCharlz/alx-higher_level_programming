4709d20c8689c28b47062631003486603150b18f
“Charles <charlznwoko@yahoo.com>
2023-11-17 00:32:17 -0800

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
 """
-Contains State class and Base, an instance of declarative_base()
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
 """
-from sqlalchemy import Column, Integer, String, MetaData
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class State(Base):
-    """
-    Class with id and name attributes of each state
+    """State class
+
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
+        cities (:obj:`City`): The Cities belongs to State
+
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
+    cities = relationship("City", backref="state", cascade="all, delete")
